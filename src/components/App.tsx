52e3a4f0f7c2a5f5310a2db629f3a6fd41bfbba7
Rafał Karczmarzyk <karczmarzyk.rafal@gmail.com>
2022-06-08 16:45:37 +0200

    Basic rendering

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,50 @@
-import React from "react";
-import MazeCanvas from "./MazeCanvas/MazeCanvas";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { mazeGenerator } from '../libs/maze/maze';
+import { Maze } from '../libs/maze/types';
+import MazeCanvas from './MazeCanvas/MazeCanvas';
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-gap: 5px;
+`;
 
 function App() {
-  return <MazeCanvas />;
+  const [seed, setSeed] = useState('test');
+  const [width, setWidth] = useState(32);
+  const [height, setHeight] = useState(32);
+  const [maze, setMaze] = useState<Maze>([]);
+
+  useEffect(() => {
+    const mazeGen = mazeGenerator({ width, height, seed });
+    for (let i = 0; i < 100; i++) {
+      mazeGen.next();
+    }
+    const maze = mazeGen.next().value as Maze;
+    setMaze(maze);
+  }, [seed, width, height]);
+
+  return (
+    <Wrapper>
+      <label>Seed:</label>
+      <input value={seed} onChange={(e) => setSeed(e.target.value)} />
+      <label>Width:</label>
+      <input
+        value={width}
+        type="number"
+        onChange={(e) => setWidth(Number(e.target.value))}
+      />
+      <label>Height:</label>
+      <input
+        value={height}
+        type="number"
+        onChange={(e) => setHeight(Number(e.target.value))}
+      />
+
+      <button>Generate</button>
+      <MazeCanvas maze={maze} />
+    </Wrapper>
+  );
 }
 
 export default App;
--- src/components/MazeCanvas/MazeCanvas.tsx
+++ src/components/MazeCanvas/MazeCanvas.tsx
@@ -1,32 +1,56 @@
 import React, { FC, useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import { mazeGenerator } from '../../libs/maze/maze';
+import { Maze, MazeCellType } from '../../libs/maze/types';
 
 const Wrapper = styled.div`
-    padding: 15px;
+  padding: 15px;
 `;
 const Canvas = styled.canvas`
-    outline: 1px solid black;
+  outline: 1px solid black;
 `;
 
-const MazeCanvas: FC = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+interface Props {
+  maze?: Maze;
+}
 
-    useEffect(() => {
-        if (canvasRef.current) {
-            const ctx = canvasRef.current.getContext('2d');
-            if (ctx) {
-                ctx.canvas.width = 600;
-                ctx.canvas.height = 600;
-                ctx.fillStyle = '#FF0000';
-                ctx.fillRect(0, 0, 16, 16);
+const MazeCanvas: FC<Props> = ({ maze = [] }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext('2d');
+      if (ctx) {
+        ctx.canvas.width = 600;
+        ctx.canvas.height = 600;
+        maze.forEach((yRow, y) => {
+          yRow.forEach((cell, x) => {
+            if (cell === MazeCellType.Empty) {
+              ctx.fillStyle = '#EEEEEE';
             }
-        }
-    }, [canvasRef]);
-    return (
-        <Wrapper>
-            <Canvas ref={canvasRef} />
-        </Wrapper>
-    );
+            if (cell === MazeCellType.EmptyVisited) {
+              ctx.fillStyle = '#0000EE';
+            }
+            if (cell === MazeCellType.Wall) {
+              ctx.fillStyle = '#333333';
+            }
+            if (cell === MazeCellType.Entrance) {
+              ctx.fillStyle = '#00FF00';
+            }
+            if (cell === MazeCellType.Exit) {
+              ctx.fillStyle = '#FF0000';
+            }
+            ctx.fillRect(x * 16, y * 16, 16, 16);
+          });
+        });
+      }
+    }
+  }, [canvasRef, maze]);
+  return (
+    <Wrapper>
+      <Canvas ref={canvasRef} />
+    </Wrapper>
+  );
 };
 
 export default MazeCanvas;
